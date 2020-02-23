ae93f39219eb204b2cf1c1d1098677cb9e4e2841
Frank Hakim <G19H0850@ict.ru.ac.za>
2020-02-23 15:24:27 +0200

    I have tested my cases

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Problems;
 
 namespace Problems.Tests
 {
@@ -12,7 +13,40 @@ namespace Problems.Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+           
+            Assert.IsNotEmpty(Program.Prefix("Hello"));
         }
+
+        [Test]
+        public void IsNull()
+        {
+            object nada = null;
+
+            // Classic syntax
+            Assert.IsNull(nada);
+
+            // Constraint Syntax
+            //Assert.That(nada, Is.Null);
+        }
+
+          [Test]
+        public void Test2()
+        {
+           
+            Assert.IsNotEmpty(Program.Prefix(""));
+        }
+
+          [Test]
+        public void Test3()
+        {
+           
+            Assert.IsNotEmpty(Program.Prefix("what ... did you say??")); 
+        }
+
+
+        
+
+        
+
     }
 }
--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Problems
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Prefix("Hello"));
+		}
+
+		public static string Prefix(String x)
+		{
+			int length = x.Length;
+			string[] words = x.Split(' ');
+			int wrd = words.Length;
+			
+			return(length + "," + wrd + ":" + x);
+		}
+	}
 }
